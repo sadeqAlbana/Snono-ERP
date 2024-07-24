c899832da03e77baa38ae78f641e0eda76c17e40
sadeq <sadeq.albana@gmail.com>
2024-07-25 02:04:57 +0300

    fixed typo

--- app/models/posmachinesmodel.cpp
+++ app/models/posmachinesmodel.cpp
@@ -2,7 +2,7 @@
 
 PosMachinesModel::PosMachinesModel(QObject *parent) : AppNetworkedJsonModel("/postmachines",{
                                               {"id",tr("ID")} ,
-                                              {"hd_id",tr("Hardware ID")} ,
+                                              {"hw_id",tr("Hardware ID")} ,
                                               {"ip_address",tr("IP Address")} ,
                                               {"account_id",tr("Account ID")} ,
                                               {"enabled",tr("Enabled"),QString(),false},
