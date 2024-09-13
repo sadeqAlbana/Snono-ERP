57c742986abcf7370c34f1464c753b720d84c42c
sadeq <sadeq.albana@gmail.com>
2024-09-13 06:48:47 +0300

    fixed order display model

--- app/models/onlineordersmodel.cpp
+++ app/models/onlineordersmodel.cpp
@@ -130,7 +130,11 @@ QVariant OnlineOrdersModel::data(const QModelIndex &index, int role) const
     auto key = m_columns.value(index.column());
     if(key.m_key=="district" && role==Qt::DisplayRole){
         QJsonObject record=jsonObject(index.row()).value("shipment").toObject().value("dst_address").toObject();
-        return QString("%1 - %2 - %3").arg(record.value("province").toString()).arg(record.value("district").toString()).arg(record.value("details").toString());
+        QString combo=QString("%1 - %2").arg(record.value("province").toString()).arg(record.value("district").toString());
+        if(record.value("details").toString().size()){
+            combo.append(QString(" - %1").arg(record.value("details").toString()));
+        }
+        return combo;
     }
 
     return AppNetworkedJsonModel::data(index,role);
