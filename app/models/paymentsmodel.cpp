b35f1e118428949aee6bfe0efc72327831aaffe8
sadeq <sadeq.albana@gmail.com>
2025-02-04 03:44:35 +0300

    working on payments

--- app/models/paymentsmodel.cpp
+++ app/models/paymentsmodel.cpp
@@ -1,5 +1,15 @@
 #include "paymentsmodel.h"
 
-PaymentsModel::PaymentsModel(QObject *parent)
-    : AppNetworkedJsonModel{parent}
-{}
+
+PaymentsModel::PaymentsModel(QObject *parent) : AppNetworkedJsonModel("/payments",{
+                                                 {"id",tr("ID")} ,
+                                                 {"name",tr("Payment Method"),"payment_method"} ,
+                                                 {"journal_entry_id",tr("Journal Entry")} ,
+                                                 {"date",tr("Date"),QString(),false},
+                                                 {"created_at",tr("Created At"),QString(),false}
+
+                                             },
+                            parent)
+{
+
+}
