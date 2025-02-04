e170b8791748a277157bdf31a67704039857d8c3
sadeq <sadeq.albana@gmail.com>
2025-02-04 03:53:41 +0300

    working on invocies view

--- app/models/invoicesmodel.cpp
+++ app/models/invoicesmodel.cpp
@@ -1,5 +1,19 @@
 #include "invoicesmodel.h"
 
-InvoicesModel::InvoicesModel(QObject *parent)
-    : AppNetworkedJsonModel{parent}
-{}
+
+
+InvoicesModel::InvoicesModel(QObject *parent) : AppNetworkedJsonModel("/payments",{
+                                             {"id",tr("ID")} ,
+                                             {"name",tr("Name")} ,
+                                             {"status",tr("Status")} ,
+                                             {"date",tr("Date"),QString(),false},
+                                             {"due_date",tr("Due Date"),QString(),false},
+                                             {"balance_due",tr("Balance Due"),QString(),false},
+                                             {"Total",tr("Total"),QString(),false},
+                                             {"created_at",tr("Created At"),QString(),false}
+
+                                         },
+                            parent)
+{
+
+}
